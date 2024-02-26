e6c612ae8848178e1920cb2a48e83d8a13872f5f
Vvatte <vvatte@yahoo.com>
2024-02-26 19:24:15 -0300

    calculando media

---
+++ calculo-media/src/CalculoMedia.java
@@ -0,0 +1,15 @@
+public class CalculoMedia {
+
+    public static void main(String[] args){
+
+        int num1, num2, num3, num4, media;
+
+        num1= 14;
+        num2= 17;
+        num3= 9;
+        num4= 20;
+
+        media =(num1 + num2 + num3 + num4) / 4;
+        System.out.println("Total" + media);
+    }
+}
